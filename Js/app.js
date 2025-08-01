1d7a7f38e5d9a6b98a97d7930fc75c3311e51ffb
Daniel Zamora Paez <[email>
2025-08-01 15:33:40 -0500

    Funcionalidad del boton de like completada

--- Js/app.js
+++ Js/app.js
@@ -1,14 +1,14 @@
-const likeBoton = document.getElementById('likeBoton');
-const likeContador = document.getElementsByName('likeContador');
- 
-let count = 0;
- 
-likeBoton.addEventListener('click', () => {
-  count++;
-  likeContador.textContent = count;
- 
-  // Cambiar texto del botón solo en el primer clic
-  if (count === 1) {
-    likeBoton.textContent = '¡Gracias por tu like!';
-  }
+const botonesLike = document.querySelectorAll('.card .boton');
+botonesLike.forEach(boton => {
+    const originalContent = boton.innerHTML;
+    let isLiked = false;
+
+    boton.addEventListener('click', () => {        
+        isLiked = !isLiked;
+        if (isLiked) {
+            boton.textContent = '¡Gracias por tu like!';
+        } else {
+            boton.innerHTML = originalContent;
+        }
+    });
 });
